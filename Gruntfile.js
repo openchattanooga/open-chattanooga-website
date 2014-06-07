aa78ea19b81575a9a13c46ffc6afd6789d874bec
Daniel Ryan <dryan@dryan.com>
2014-06-07 15:53:10 -0400

    bower/grunt build system for static assets

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports  =   function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'css/screen.css': 'sass/screen.scss'
+                }
+            },
+            options: {
+                loadPath: [
+                    'bower_components/bourbon/dist',
+                    'bower_components/bootstrap-sass-official/vendor/assets/stylesheets'
+                ],
+                sourcemap: true,
+                style: 'compressed',
+                banner: '/*! <%= pkg.name %> <%= pkg.version %> | <%= pkg.license %> | <%= pkg.homepage %> */'
+            }
+        },
+        jshint: {
+            files: [
+                'Gruntfile.js'
+            ],
+            jshintrc: '.jshintrc'
+        },
+        uglify: {
+            options: {
+                sourceMap: true,
+                banner: '/*! <%= pkg.name %> <%= pkg.version %> | <%= pkg.license %> | <%= pkg.homepage %> */'
+            },
+            dist: {
+                files: {
+                    'js/core.min.js': ['js/core.js']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'sass/*.scss',
+                tasks: ['sass']
+            },
+            js: {
+                files: [
+                    'Gruntfile.js'
+                ],
+                tasks: ['jshint', 'uglify']
+            }
+        },
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                commit: true,
+                commitMessage: 'Release %VERSION%',
+                commitFiles: ['-a'],
+                createTag: true,
+                tagName: '%VERSION%',
+                tagMessage: 'Release %VERSION%',
+                push: true,
+                pushTo: 'gh-pages'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['sass', 'jshint', 'uglify']);
+    grunt.registerTask('rev:patch', ['bump-only:patch', 'sass', 'jshint', 'uglify', 'bump-commit']);
+    grunt.registerTask('rev:minor', ['bump-only:minor', 'sass', 'jshint', 'uglify', 'bump-commit']);
+    grunt.registerTask('rev:major', ['bump-only:major', 'sass', 'jshint', 'uglify', 'bump-commit']);
+    grunt.registerTask('rev',       ['rev:patch']);
+};
